0e443268b95067696946e914a5abad734085f522
Renat19Belei <resyaresya1908@gmail.com>
2025-09-23 17:57:47 +0300

    homework

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+
+const app = express();
+const HOST = "127.0.0.1"
+const PORT = 8000;
+
+const postsFilePath = path.join(__dirname, 'posts.json');
+const posts = JSON.parse(fs.readFileSync(postsFilePath, 'utf8'));
+
+app.get('/posts', (req, res) => {
+  res.status(200).json(posts);
+});
+
+app.listen(PORT, () => {
+  console.log(`Серверр запущено на http://localhost:${PORT}/`);
+});
